003b9ae50a00bed7d457d11f39d7d4aee73b6f00
1FOGOK1 <fogok.production@gmail.com>
2017-11-01 04:32:52 +0300

    maybe server architect

--- src/com/fogok/spaceshipserver/NettyHandler.java
+++ src/com/fogok/spaceshipserver/NettyHandler.java
@@ -2,29 +2,27 @@ package com.fogok.spaceshipserver;
 
 import com.fogok.spaceshipserver.logic.LogicThreadPool;
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.group.ChannelGroup;
-import io.netty.channel.group.DefaultChannelGroup;
-import io.netty.util.concurrent.GlobalEventExecutor;
 
-class NettyHandler extends ChannelInboundHandlerAdapter {
+public class NettyHandler extends ChannelInboundHandlerAdapter {
 
-    private final static String encoding = "UTF-8";
-    private final static ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+    public final static String encoding = "UTF-8";
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+
+    }
 
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
-        channels.add(ctx.channel());
+
     }
 
     @Override
     public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("Client has left");
-        channels.remove(ctx.channel());
+        System.out.println(String.format("%s has left", LogicThreadPool.getInstance().getLoginsClients().get(ctx.channel().hashCode()).getLogin()));
+        LogicThreadPool.getInstance().clientLeft(ctx.channel());
     }
 
     @Override
@@ -36,28 +34,17 @@ class NettyHandler extends ChannelInboundHandlerAdapter {
 
             String json = new String(req, encoding);
             if (json.charAt(0) == 'l') { //first read, it is login pass     json == "l USERNAME"
-                LogicThreadPool.getInstance().clientAdd(json);
-                System.out.println("Client has joined. Login: %s" + json.split(" ")[1]);
-            } else {
-                Channel incoming = ctx.channel();
-                for (Channel channel : channels) {
-                    if (channel != incoming) {  //return data to all chanells
-                        channel.writeAndFlush(Unpooled.copiedBuffer(json.getBytes()));
-                    }
-                }
-                System.out.println("Client response:" + json);
-            }
+                String login = json.split(" ")[1];
+                LogicThreadPool.getInstance().clientAdd(new LogicThreadPool.LogicData(ctx.channel(), login, json));
+                System.out.println(String.format("%s has joined", login));
+            } else
+                LogicThreadPool.getInstance().clientHandle(ctx.channel().hashCode(), json);     //тупо рефрешим данные
+
 
         } finally {
             buf.release();
         }
     }
-
-
-    private void buisnessLogic(String json){
-
-    }
-
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
--- src/com/fogok/spaceshipserver/logic/LogicThreadPool.java
+++ src/com/fogok/spaceshipserver/logic/LogicThreadPool.java
@@ -1,10 +1,14 @@
 package com.fogok.spaceshipserver.logic;
 
+import com.fogok.spaceshipserver.NettyHandler;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 
-import java.util.ArrayList;
-import java.util.concurrent.ExecutorService;
+import java.nio.charset.Charset;
+import java.util.HashMap;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class LogicThreadPool {
 
@@ -14,35 +18,91 @@ public class LogicThreadPool {
         return instance == null ? instance = new LogicThreadPool() : instance;
     }
 
-    private final StringBuffer stringBuffer;
-    private final ArrayList<String> loginsClients;  //todo: все говно, хз как тут сделать
-    private final ExecutorService es;
+    public static class LogicData{
+        private Channel channel;
+        private String json;
+        private String login;
+        public LogicData(final Channel channel, final String login, final String json) {
+            this.channel = channel;
+            this.login = login;
+            updateJson(json);
+        }
 
-    private  LogicThreadPool() {
-        es = Executors.newFixedThreadPool(4);
-        loginsClients = new ArrayList<>();
-        stringBuffer = new StringBuffer();
+        public void updateJson(final String json) {
+            this.json = json;
+        }
+
+        public Channel getChannel() {
+            return channel;
+        }
+
+        public String getJson() {
+            return json;
+        }
+
+        public String getLogin() {
+            return login;
+        }
     }
 
-    public void clientAdd(final String json) {
-        loginsClients.add(json);
+    private static final char[] JSONElements = new char[]{
+         //  0    1    2    3    4    5    6     7
+            '{', '}', ':', '"', ',', 'N', '[', ']'
+    };
+
+    private final StringBuffer fatJson;
+    private final HashMap<Integer, LogicData> loginsClients;
+    private final ScheduledExecutorService service;
+
+    private LogicThreadPool() {
+        service = Executors.newSingleThreadScheduledExecutor();
+        loginsClients = new HashMap<>(1000); //хз как >1к коннектов тут может быть
+        fatJson = new StringBuffer();
+        service.scheduleAtFixedRate(() -> {                     // нет рефактора, все сыро и на коленке, чисто для прототипа!
+            if (loginsClients.values().size() != 0) {
+                //main logic
+
+                for (LogicData logicData : loginsClients.values()) {
+//                    buildJson(logicData.channel.hashCode());  //TODO: revert to this line
+                    fatJson.setLength(0);
+                    fatJson.append("[{\"1\":[{\"x\":10.0,\"y\":5.62,\"a\":[0.0,0.0,1.4]}]},{\"1\":[{\"x\":14.0,\"y\":2.62,\"a\":[0.0,0.0,1.4]}]},{\"0\":[{\"x\":7.46,\"y\":5.85,\"a\":[0.003,3.349,346.0]},{\"x\":6.212,\"y\":6.638,\"a\":[0.003,3.195,393.0]},{\"x\":4.31,\"y\":6.008,\"a\":[0.003,3.039,98.0]},{\"x\":4.176,\"y\":4.595,\"a\":[0.003,2.886,148.0]},{\"x\":5.1,\"y\":3.717,\"a\":[0.003,2.766,163.0]},{\"x\":6.654,\"y\":3.472,\"a\":[0.003,2.577,175.0]},{\"x\":7.511,\"y\":3.412,\"a\":[0.003,2.475,177.0]},{\"x\":9.715,\"y\":10.881,\"a\":[0.003,0.053,412.0]},{\"x\":11.348,\"y\":10.108,\"a\":[0.003,0.19,362.0]},{\"x\":12.343,\"y\":7.808,\"a\":[0.003,0.327,312.0]},{\"x\":10.784,\"y\":6.589,\"a\":[0.003,0.481,255.0]},{\"x\":10.371,\"y\":5.533,\"a\":[0.003,0.618,265.0]},{\"x\":12.645,\"y\":5.059,\"a\":[0.003,0.772,322.0]},{\"x\":13.821,\"y\":5.99,\"a\":[0.003,0.927,363.0]},{\"x\":14.991,\"y\":7.022,\"a\":[0.003,1.082,397.0]},{\"x\":15.967,\"y\":7.036,\"a\":[0.003,1.219,370.0]},{\"x\":17.128,\"y\":4.955,\"a\":[0.003,1.372,313.0]},{\"x\":15.785,\"y\":3.745,\"a\":[0.003,1.53,257.0]},{\"x\":13.873,\"y\":3.39,\"a\":[0.003,1.679,202.0]},{\"x\":11.404,\"y\":3.247,\"a\":[0.003,1.987,191.0]},{\"x\":10.142,\"y\":2.974,\"a\":[0.003,2.141,190.0]},{\"x\":8.615,\"y\":3.394,\"a\":[0.003,2.322,180.0]}],\"1\":[{\"x\":9.951,\"y\":7.711,\"a\":[280.203,0.0,1.4]}]}]");   //stressTEst
+//                    fatJson.append("[{\"1\":[{\"x\":10.0,\"y\":5.62,\"a\":[0.0,0.0,1.4]}]},{\"1\":[{\"x\":14.0,\"y\":2.62,\"a\":[0.0,0.0,1.4]}]}]"); //easy Test
+                    logicData.channel.writeAndFlush(Unpooled.copiedBuffer(fatJson.toString().getBytes(Charset.forName(NettyHandler.encoding))));
+                }
+
+
+            }
+        }, 0, 16, TimeUnit.MILLISECONDS);
+    }
+
+    private void buildJson(int hashcodesendchannel){
+        fatJson.setLength(0);
+        fatJson.append(JSONElements[6]);
+        int iters = 0;
+        for (LogicData logicData : loginsClients.values()) { //handle all channels
+            if (logicData.channel.hashCode() != hashcodesendchannel) {
+                if (iters++ != 0)
+                    fatJson.append(JSONElements[4]);
+                fatJson.append(logicData.json);
+            }
+        }
+        fatJson.append(JSONElements[7]);
+    }
+
+    public void clientAdd(final LogicData logicData) {
+        loginsClients.put(logicData.getChannel().hashCode(), logicData);
+    }
+
+    public void clientHandle(int hashcodeChannel, final String json){
+        loginsClients.get(hashcodeChannel).updateJson(json);
     }
 
     public void clientLeft(final Channel channel) {
-
+        loginsClients.remove(channel.hashCode());
     }
 
-    public void handle(final String json) {
-        es.execute(() -> {
-            try {
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                throw new RuntimeException(e);
-            }
-        });
+    public HashMap<Integer, LogicData> getLoginsClients() {
+        return loginsClients;
     }
-
-
 }
 
