ba4a6bbc91604a8f4ab9e56135ca12c92e118015
FOGOK <fogok.production@gmail.com>
2017-12-03 15:33:06 +0300

    test

--- src/main/java/com/fogok/spaceshipserver/utlis/Console.java
+++ src/main/java/com/fogok/spaceshipserver/utlis/Console.java
@@ -2,11 +2,9 @@ package com.fogok.spaceshipserver.utlis;
 
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
-import com.fogok.dataobjects.GameObjectsType;
-import com.fogok.dataobjects.gameobjects.ships.SimpleShipObject;
+import com.fogok.dataobjects.ServerState;
 import com.fogok.dataobjects.utils.EveryBodyPool;
 import com.fogok.dataobjects.utils.Serialization;
-import com.fogok.spaceshipserver.game.EverybodyObjectsController;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -15,39 +13,55 @@ import java.io.IOException;
 public class Console {
 
     public static void main(String[] args) throws IOException {
-        EverybodyObjectsController serverEVB = new EverybodyObjectsController();
-        EveryBodyPool clientPool = new EveryBodyPool(100);
-        Serialization.getInstance().setParams(clientPool);
-
-        serverEVB.getEveryBodyObjectsPool().obtain(GameObjectsType.SimpleBluster);
-        serverEVB.getEveryBodyObjectsPool().obtain(GameObjectsType.SimpleBluster);
-        serverEVB.getEveryBodyObjectsPool().obtain(GameObjectsType.SimpleBluster);
-        serverEVB.getEveryBodyObjectsPool().obtain(GameObjectsType.SimpleBluster);
-        serverEVB.getEveryBodyObjectsPool().obtain(GameObjectsType.SimpleBluster);
-        serverEVB.getEveryBodyObjectsPool().obtain(GameObjectsType.SimpleBluster);
-
-        serverEVB.getEveryBodyObjectsPool().getAllObjectsFromType(GameObjectsType.SimpleShip).peek().setAdditParam(0.3f, SimpleShipObject.AdditParams.DIRECTION);
+//        EverybodyObjectsController serverEVB = new EverybodyObjectsController();
+//        EveryBodyPool clientPool = new EveryBodyPool(100);
+//        Serialization.getInstance().setParams(clientPool);
+//
+//        serverEVB.getEveryBodyObjectsPool().obtain(GameObjectsType.SimpleBluster);
+//        serverEVB.getEveryBodyObjectsPool().obtain(GameObjectsType.SimpleBluster);
+//        serverEVB.getEveryBodyObjectsPool().obtain(GameObjectsType.SimpleBluster);
+//        serverEVB.getEveryBodyObjectsPool().obtain(GameObjectsType.SimpleBluster);
+//        serverEVB.getEveryBodyObjectsPool().obtain(GameObjectsType.SimpleBluster);
+//        serverEVB.getEveryBodyObjectsPool().obtain(GameObjectsType.SimpleBluster);
+//
+//        serverEVB.getEveryBodyObjectsPool().getAllObjectsFromType(GameObjectsType.SimpleShip).peek().setAdditParam(0.3f, SimpleShipObject.AdditParams.DIRECTION);
+//
+//        Output output = new Output(new ByteArrayOutputStream());
+//        Input input = new Input(new ByteArrayInputStream(new byte[4096]));
+//
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println("Start " + i);
+//
+//            //
+//            Serialization.getInstance().getKryo().writeObject(output, serverEVB.getEveryBodyObjectsPool());
+//            //
+//
+//            //
+//            input.setBuffer(output.getBuffer());
+//            Serialization.getInstance().getKryo().readObject(input, EveryBodyPool.class);
+//            //
+//
+//            System.out.println(String.format("Client %s", clientPool.toString(false)));
+//        }
+//
+//        output.close();
+//        output.clear();
 
         Output output = new Output(new ByteArrayOutputStream());
         Input input = new Input(new ByteArrayInputStream(new byte[4096]));
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Start " + i);
+        ServerState serverState = new ServerState();
 
-            //
-            Serialization.getInstance().getKryo().writeObject(output, serverEVB.getEveryBodyObjectsPool());
-            //
+        //??????
+//        serverState...setParams
+        Serialization.getInstance().setServerState(serverState);
+        Serialization.getInstance().getKryo().writeObject(output, serverState);
+        input.setBuffer(output.getBuffer());
 
-            //
-            input.setBuffer(output.getBuffer());
-            Serialization.getInstance().getKryo().readObject(input, EveryBodyPool.class);
-            //
+        //??????
+        Serialization.getInstance().setServerState(serverState);
+        Serialization.getInstance().getKryo().readObject(input, EveryBodyPool.class);
 
-            System.out.println(String.format("Client %s", clientPool.toString(false)));
-        }
-
-        output.close();
-        output.clear();
 
 
     }
